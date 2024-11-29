28b227d60898a0a0c588aad993c228573dcf6e63
peterGeswint <petergaeswint@outlook.com>
2024-11-29 21:30:19 +0000

    task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  return students
+    .filter((student) => student.location === city) // Filter students by city
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A', // Assign grade or N/A
+      };
+    });
+}
