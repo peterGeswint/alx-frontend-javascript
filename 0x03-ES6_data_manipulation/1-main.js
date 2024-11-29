a90e582cf9403bd73aeffbc8aa30c6c8d34c19ff
peterGeswint <petergaeswint@outlook.com>
2024-11-29 20:25:28 +0000

    task

---
+++ 0x03-ES6_data_manipulation/1-main.js
@@ -0,0 +1,6 @@
+import getListStudentIds from './1-get_list_student_ids';
+import getListStudents from './0-get_list_students';
+
+// Test cases
+console.log(getListStudentIds('hello')); // Should print []
+console.log(getListStudentIds(getListStudents())); // Should print [1, 2, 5]
