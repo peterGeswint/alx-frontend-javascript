a6f0e516957b361729b53384a9a163b9b25fd5ea
peterGeswint <petergaeswint@outlook.com>
2024-11-29 22:25:10 +0000

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,15 @@
 export default function cleanSet(set, startString) {
-  // Filter the set values that start with startString and append the rest of the string
+  
+  if (typeof startString !== 'string') {
+    return '';
+  }
+
+  if (startString === '') {
+    return [...set].join('-');
+  }
+
   return [...set]
-    .filter((value) => value.startsWith(startString)) // Filter values starting with startString
-    .map((value) => value.slice(startString.length)) // Remove the prefix from each matching value
-    .join('-'); // Join the results with a hyphen
+    .filter(value => value.startsWith(startString)) 
+    .map(value => value.slice(startString.length))   
+    .join('-');                                      
 }
